54ddb6b2d20e2cc0dc674db9c04cabc25e4dc203
ZubrikPi <zubrikpi@gmail.com>
2020-10-04 20:01:36 +0300

    Update headfirst_go.go

--- headfirst_go.go
+++ headfirst_go.go
@@ -33,19 +33,61 @@
 //   fmt.Println(lenght*width, "square meters")
 // }
 
+// package main
+// import "fmt"
+// func main() {
+//   var quantity int
+//   var lenght, width float64
+//   var customerName string
+//
+//   quantity = 4
+//   lenght, width = 1.2, 2.4
+//   customerName = "Damon Cole"
+//
+//   fmt.Println(customerName)
+//   fmt.Println("has ordered", quantity, "sheets")
+//   fmt.Println("each with an area of")
+//   fmt.Println(lenght*width, "square meters")
+// }
+
+// package main
+// import "fmt"
+// func main() {
+//   var quantity int            = 4
+//   var lenght, width float64   = 1.2, 2.4
+//   var customerName string     = "Damon Cole"
+//   //
+//   // quantity = 4
+//   // lenght, width = 1.2, 2.4
+//   // customerName = "Damon Cole"
+//   //
+//   fmt.Println(customerName)
+//   fmt.Println("has ordered", quantity, "sheets")
+//   fmt.Println("each with an area of")
+//   fmt.Println(lenght*width, "square meters")
+// }
+
 package main
 import "fmt"
 func main() {
-  var quantity int
-  var lenght, width float64
-  var customerName string
-
-  quantity = 4
-  lenght, width = 1.2, 2.4
-  customerName = "Damon Cole"
+  quantity          := 4
+  lenght, width     := 1.2, 2.4
+  customerName      := "Damon Cole"
 
   fmt.Println(customerName)
   fmt.Println("has ordered", quantity, "sheets")
   fmt.Println("each with an area of")
   fmt.Println(lenght*width, "square meters")
 }
+
+// // задание "Развлечения с магнитами"
+// package main
+// import "fmt"
+// func main() {
+//   var originalCount int = 10
+//   var eatenCount    int = 4
+//
+//   fmt.Println("I starter with", originalCount, "apples")
+//   fmt.Println("Some jerk ate", eatenCount, "apples")
+//   fmt.Println("There are", originalCount-eatenCount, "apples left")
+// }
