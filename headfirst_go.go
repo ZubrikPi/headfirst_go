f533f3a5cb97929423598000464c5a7dcc6c83e6
ZubrikPi <zubrikpi@gmail.com>
2020-10-06 11:25:25 +0300

    Update headfirst_go.go

--- headfirst_go.go
+++ headfirst_go.go
@@ -67,27 +67,27 @@
 //   fmt.Println(lenght*width, "square meters")
 // }
 
-package main
-import "fmt"
-func main() {
-  quantity          := 4
-  lenght, width     := 1.2, 2.4
-  customerName      := "Damon Cole"
-
-  fmt.Println(customerName)
-  fmt.Println("has ordered", quantity, "sheets")
-  fmt.Println("each with an area of")
-  fmt.Println(lenght*width, "square meters")
-}
-
-// // задание "Развлечения с магнитами"
 // package main
 // import "fmt"
 // func main() {
-//   var originalCount int = 10
-//   var eatenCount    int = 4
+//   quantity          := 4
+//   lenght, width     := 1.2, 2.4
+//   customerName      := "Damon Cole"
 //
-//   fmt.Println("I starter with", originalCount, "apples")
-//   fmt.Println("Some jerk ate", eatenCount, "apples")
-//   fmt.Println("There are", originalCount-eatenCount, "apples left")
+//   fmt.Println(customerName)
+//   fmt.Println("has ordered", quantity, "sheets")
+//   fmt.Println("each with an area of")
+//   fmt.Println(lenght*width, "square meters")
 // }
+
+// задание "Развлечения с магнитами"
+package main
+import "fmt"
+func main() {
+  var originalCount int = 10
+  var eatenCount    int = 4
+
+  fmt.Println("I starter with", originalCount, "apples")
+  fmt.Println("Some jerk ate", eatenCount, "apples")
+  fmt.Println("There are", originalCount-eatenCount, "apples left")
+}
